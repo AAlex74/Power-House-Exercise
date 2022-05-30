2efd070e5f27de40d0362b47f00f259e0152bc59
April A <aamalex13@gmail.com>
2022-05-29 19:52:42 -0500

    my readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Power House Exercise
+
+This is an exercise app that will track the exercisese and the mins on each exercise. Is using Node.js Express and MySQL.
--- app.js
+++ app.js
@@ -19,7 +19,8 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
-});
+});EADME.md
+
 
 app.use(logger('dev'));
 app.use(express.json());
